5f8623d8cdd74dee06b4ac32a9ab695b980f35c7
NeilR <nruggier@gmail.com>
2018-01-21 13:54:34 -0500

    Project Submission

--- main.cpp
+++ main.cpp
@@ -1,165 +1,227 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <string.h>
-#include <netdb.h>
+#include <cstdlib>
 #include <string>
-#include <unistd.h>
-#include <zlib.h>
 #include <algorithm>
 #include "connection.h"
 
-#define MAGIC_VALUE 53545259
 #define MAX_BUFFER_SIZE 10000 // max payload size of 10KiB
-#define CODE_SIZE 2           // 2 bytes
 #define PAYLOAD_TOO_LARGE 32
 
 class connection;
 
 // forward declarations
 void parse_message();
+
 void ping();
-void get_stats() ;
-void reset_stats() ;
-void error(int error_code) ;
-void compression() ;
+
+void get_stats();
+
+void reset_stats();
+
+void error(int error_code);
+
+void compression();
 
 using namespace std;
 
 // set up all the data **************************
 char buffer[MAX_BUFFER_SIZE];
-char out_buffer[MAX_BUFFER_SIZE];
 int sockfd;
-const void *magic_value_string = "53545259";
 connection connection_socket;
-uint32_t magic_value;
-uint16_t payload_length;
-uint16_t code;
-uint32_t total_bytes_received = 0;
-uint32_t total_bytes_sent = 0;
+string magic_value;
+string payload_length;
+string code;
+string magic_value_string = "53545259";
 string payload;
-int request_codes[] = {1, 2, 3, 32};
+int int_code = 0;
+int int_payload_length = 0;
+int total_bytes_received = 0;
+int total_bytes_sent = 0;
 /*************************************************/
 
+int main() {
 
-int main(void) {
     // set up the socket file descriptor
     sockfd = connection_socket.setup_connection();
 
-    // while loop keeps server looking for incoming data
-    while(1) {
-        // retrieve the incoming data
-        connection_socket.receive_data(sockfd, buffer, MAX_BUFFER_SIZE);
+    // retreive the incoming data until successful
+    connection_socket.receive_data(sockfd, buffer, MAX_BUFFER_SIZE);
 
-        string message = buffer;
+    string message = buffer;
 
-        // parse the message from the buffer
-        parse_message();
+    // parse the message from the buffer
+    parse_message();
 
-        if (magic_value != MAGIC_VALUE) {
-            perror("Invalid Magic Number");
-        }
+    if (magic_value != magic_value_string) {
+        perror("Invalid Magic Number");
+    }
 
-        if (payload_length > MAX_BUFFER_SIZE) {
-            code = PAYLOAD_TOO_LARGE;
-        }
+    if (int_payload_length > MAX_BUFFER_SIZE) {
+        code = PAYLOAD_TOO_LARGE;
+    }
 
-        switch(code) {
-            case 1:
-                ping();
-                break;
-            case 2:
-                get_stats();
-                break;
-            case 3:
-                reset_stats();
-                break;
-            case 4:
-                compression();
-                break;
-        }
-
+    switch (int_code) {
+        case 1:
+            ping();
+            break;
+        case 2:
+            get_stats();
+            break;
+        case 3:
+            reset_stats();
+            break;
+        case 4:
+            compression();
+            break;
+        default:
+            break;
     }
 }
 
-// ********************************************************************************************************************
 void parse_message() {
-    // copy first 4 bytes of buffer, which should be 53545259 (from 0x53545259)
-    memcpy(&magic_value, &buffer, sizeof(magic_value));
-
-    // copy first 2 bytes starting at 4 (after magic value)
-    memcpy(&payload_length, &buffer + 4, sizeof(payload_length));
-
-    // copy first 2 elements starting at 6 (after magic value and payload)
-    memcpy(&code, &buffer + 6, sizeof(code));
-
-    //copy payload_length bytes, starting at 8 (after magic value, payload_length and code)
-    memcpy(&payload, &buffer + 8, payload_length);
-
-    // if payload is too large, send appropriate response
-    if (payload_length > MAX_BUFFER_SIZE) {
-        error(2);
-    } else if (std::find(std::begin(request_codes), std::end(request_codes), code)) {
-        error(3);
+    // copy first 4 bytes of buffer, which should be 53545259
+    for (int i = 0; i < 8; i++) {
+        magic_value += buffer[i];
     }
 
-    // received 8 bytes, add this to total received
-    total_bytes_received += (8 + payload_length);
+    // copy payload length
+    for (int j = 8; j < 12; j++) {
+        payload_length += buffer[j];
+    }
+    int_payload_length = std::stoi(payload_length, nullptr, 10);
+
+    // copy code
+    for (int k = 12; k < 16; k++) {
+        code += buffer[k];
+    }
+    int_code = std::stoi(code, nullptr, 10);
+
+    //copy payload_length bytes
+    for (int l = 15; l < (int_payload_length + 16); l++) {
+        payload += buffer[l];
+    }
+
+    // if payload is too large, send appropriate response
+    if (int_payload_length > MAX_BUFFER_SIZE) {
+        error(2);
+    }
+
+    total_bytes_received += (8 + int_payload_length);
 }
 
 void ping() {
-    // copy magic number into output buffer
-    memcpy(&out_buffer, &magic_value_string, sizeof(magic_value_string));
+    // copy magic number into buffer
+    for (int i = 0; i < 8; i++) {
+        buffer[i] = magic_value_string.at(i);
+    }
 
-    // copy empty payload length into output buffer, starting at index 7
-    memcpy(&out_buffer + 4, "0", 1);
+    // set payload length to 0
+    for (int j = 8; j < 12; j++) {
+        buffer[j] = payload_length.at(j - 8);      // set payload length
+    }
 
     // copy OK code (0) into code field
-    memcpy(&out_buffer + 6, "0000", CODE_SIZE);
+    code = "0000";
+    for (int k = 12; k < 16; k++) {
+        buffer[k] = code.at(k - 12);
+    }
 
     // send the message
-    connection::send_data(sockfd, out_buffer, reinterpret_cast<int *>(8));
+    connection::send_data(sockfd, buffer, 16);
     total_bytes_sent += 8;
 
 }
 
 void error(int error_code) {
     // copy magic number
-    memcpy(&out_buffer, &magic_value_string, sizeof(magic_value_string));
+    for (int i = 0; i < 8; i++) {
+        buffer[i] = magic_value_string.at(i);
+    }
 
     // set payload length to 0
-    memcpy(&out_buffer + 4, "0", 1);
+    for (int j = 8; j < 12; j++) {
+        buffer[j] = payload_length.at(j - 8);      // set payload length
+    }
 
     // set proper response code
-    memcpy(&out_buffer + 5, &error_code, sizeof(error_code));
+    code = std::to_string(error_code);
+
+    // pad code with proper amount of zeros
+    while (code.length() < 4) {
+        code = "0" + code;
+    }
+
+    for (int k = 12; k < 16; k++) {
+        buffer[k] = code.at(k - 12);
+    }
+
+    // send the message
+    connection::send_data(sockfd, buffer, 16);
+    total_bytes_sent += 8;
+    exit(0);
 }
 
 void get_stats() {
-    // copy bytes received to output buffer
-    memcpy(&out_buffer, &total_bytes_received, sizeof(total_bytes_received));
+    // convert int to string for processing
+    string bytes_received = std::to_string(total_bytes_received);
 
-    // copy bytes sent to output buffer
-    memcpy(&out_buffer + 4, &total_bytes_sent, sizeof(total_bytes_sent));
+    // prepend with proper amount of 0s
+    while (bytes_received.length() < 8) {
+        bytes_received = "0" + bytes_received;
+    }
+
+    // load data into buffer to be sent
+    for (int i = 0; i < 8; ++i) {
+        buffer[i] = bytes_received.at(i);
+    }
+
+    // convert int to string for processing
+    string bytes_sent = std::to_string(total_bytes_sent);
+
+    // prepend with proper amount of 0s
+    while (bytes_sent.length() < 8) {
+        bytes_sent = "0" + bytes_sent;
+    }
+
+    // load data into buffer to be sent
+    for (int j = 8; j < 16; ++j) {
+        buffer[j] = bytes_sent.at(j - 8);
+    }
 
     // calculate compression ratio, and add to output buffer
-    uint8_t compression_ratio = static_cast<uint8_t>((total_bytes_received / total_bytes_sent) * 100);
-    memcpy(&out_buffer + 8, &compression_ratio, sizeof(compression_ratio));
+    int compression_ratio = (total_bytes_sent / total_bytes_received) * 100;
+
+    // convert compression ratio to char
+    string str_compression_ratio = to_string(compression_ratio);
+
+    // load data into buffer to be sent
+    buffer[16] = str_compression_ratio.at(0);
+    buffer[17] = str_compression_ratio.at(1);
 
     // send the message
-    connection::send_data(sockfd, out_buffer, reinterpret_cast<int *>(9));
+    connection::send_data(sockfd, buffer, 18);
     total_bytes_sent += 9;
 }
 
 void reset_stats() {
-    // copy magic number into output buffer
-    memcpy(&out_buffer, &magic_value_string, sizeof(magic_value_string));
+    // copy magic number
+    for (int i = 0; i < 8; i++) {
+        buffer[i] = magic_value_string.at(i);
+    }
 
-    // copy status code to ouput buffer
-    memcpy(&out_buffer + 4, "0000", CODE_SIZE);
+    // set payload length to 0
+    for (int j = 8; j < 12; j++) {
+        buffer[j] = payload_length.at(j - 8);      // set payload length
+    }
+
+    // copy OK code (0) into code field
+    code = "0000";
+    for (int k = 12; k < 16; k++) {
+        buffer[k] = code.at(k - 12);
+    }
 
     // send the message
-    connection::send_data(sockfd, out_buffer, reinterpret_cast<int *>(6));
-    total_bytes_sent += 6;
+    connection::send_data(sockfd, buffer, 16);
+    total_bytes_sent += 8;
 }
 
 void compression() {
@@ -167,25 +229,41 @@ void compression() {
     int consecutive_chars = 1;          // temp variable to hold # of times a char is repeated
     string payload_builder;             // string to build up compressed payload with
 
-    for (int i = 1; i < sizeof(payload); i++) {
-        if (current_char == payload.at(i)) {     // if it matches, consecutive detected.  Increment by 1 and examine next element
-            consecutive_chars += 1;
-        } else {                                 // new character detected, build string for previous char, then move to next
-            payload_builder += std::to_string(consecutive_chars + current_char);
+    for (int i = 1; i < payload.length(); i++) {
+        if (current_char ==
+            payload.at(i)) {            // if it matches, consecutive detected.  Increment by 1 and examine next element
+            consecutive_chars++;
+        } else {                        // new character detected, build string for previous char, then move to next
+            if (consecutive_chars > 1) {
+                payload_builder = payload_builder + std::to_string(consecutive_chars);
+            }
+            payload_builder = payload_builder + current_char;
             current_char = payload.at(i);
+            consecutive_chars = 1;
         }
     }
 
+    // copy magic number
+    for (int i = 0; i < 8; i++) {
+        buffer[i] = magic_value_string.at(i);
+    }
+
+    // set up payload length
     payload_length = sizeof(payload_builder);
+    while (payload_length.length() < 5) {
+        payload_length = "0" + payload_length;
+    }
 
-    // after compression, send message out
-    memcpy(&out_buffer, &magic_value_string, sizeof(magic_value_string));
-    memcpy(&out_buffer + 4, &payload_length, sizeof(payload_length));
-    memcpy(&out_buffer + 6, &payload_builder, sizeof(payload_builder));
+    // copy payload length
+    for (int j = 8; j < 12; j++) {
+        buffer[j] = payload_length.at(j - 8);      // set payload length
+    }
 
-    connection::send_data(sockfd, out_buffer, reinterpret_cast<int *>(6 + sizeof(payload_builder)));
-    total_bytes_sent += 6 + sizeof(payload_builder);
+    // copy payload
+    for (int k = 12; k < 12 + payload_builder.length(); k++) {
+        buffer[k] = payload_builder.at(k - 12);
+    }
+
+    connection::send_data(sockfd, buffer, static_cast<int>(12 + payload_builder.length()));
+    total_bytes_sent += 6 + (payload_builder.length() / 2);
 }
-
-//TODO Implement response codes and set proper buffer for processing
-//TODO Check for only lower case letter, reject otherwise
